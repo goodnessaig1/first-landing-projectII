a0288213b2c25e0bda35ba010eb00fd1a0e177a1
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-22 08:20:53 +0100

    changed id to className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,30 +8,30 @@ function Footer() {
         <div className='footer-items'>
             
             <div className='footer-img'>
-                <list id='foot-img-1'>
+                <list className='foot-img-1'>
 
                 </list>
-                <list id='foot-img-2'>
+                <list className='foot-img-2'>
                     
                 </list>
             </div>
             <div className='footer-text'>
-            <list id='text-1'>
+            <list className='text-1'>
             About Us
                     </list>
-                    <list id='text-2'>
+                    <list className='text-2'>
             We Provide The Best
              </list> 
-             <list id='text-3'>
+             <list className='text-3'>
              Property For You.
              </list> 
-             <list id='text-4'>
+             <list className='text-4'>
              The actually assured me over and over that take little time off and come back and work. Fast forward two weeks after we are going for a new project. 
              </list> 
-             <list id='text-5'>
+             <list className='text-5'>
              The actually assured me over and over that take little time off and come back and work. Fast forward two weeks after we are going for a new project. 
              </list> 
-             <list id='text-6'>
+             <list className='text-6'>
              The actually assured me over and over that take little time off and come back and work. Fast forward two weeks after we are going for a new project. 
              </list> 
             </div>
