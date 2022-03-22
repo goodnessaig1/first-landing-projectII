e3a8e546fa2e24765303de81b3bc0d6c4b4eec44
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-03-22 08:28:32 +0100

    update

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -5,56 +5,56 @@ function Icons() {
   return (
     <div className='icon-container'>
         <div className='icon-items'>
-            <icon  id='img' />
+            <icon  className='img' />
             <list className='first-img'>
             Property Insurance
             </list>
-            <list id='property-1'>
+            <list className='property-1'>
             Nam Libera Tempore, cum soluta nobis
             </list>
-            <list id='property-2'>
+            <list className='property-2'>
             est eligondi optio cumque nihi
             </list>
-            <list id='property-3'>
+            <list className='property-3'>
             impendit.
             </list>
             <icon  id='img2' />
             <list className='second-img'>
             Lowest Commission
                 </list>
-                <list id='lowest-1'>
+                <list className='lowest-1'>
                 Nam Libera Tempore, cum soluta nobis
                 </list>
-                <list id='lowest-2'>
+                <list className='lowest-2'>
                 est eligondi optio cumque nihi
                 </list>
-                <list id='lowest-3'>
+                <list className='lowest-3'>
                 impendit.
                 </list>
-                <icon  id='img3' />
+                <icon  className='img3' />
                 <list className='third-img'>
                 Tax Advantage
                 </list>
-                <list id='tax-1'>
+                <list className='tax-1'>
                 Nam Libera Tempore, cum soluta nobis
                 </list>
-                <list id='tax-2'>
+                <list className='tax-2'>
                 est eligondi optio cumque nihi
                 </list>
-                <list id='tax-3'>
+                <list className='tax-3'>
             impendit.
             </list>
-                <icon  id='img4' />
+                <icon  className='img4' />
                 <list className='fourt-img'>
                 24 hour Consultation
                 </list>
-                <list id='hour-1'>
+                <list className='hour-1'>
                 Nam Libera Tempore, cum soluta nobis
                 </list>
-                <list id='hour-2'>
+                <list className='hour-2'>
                 est eligondi optio cumque nihi
                 </list>
-                <list id='hour-3'>
+                <list className='hour-3'>
             impendit.
             </list>
         </div>
